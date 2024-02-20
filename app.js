bb9e51ce0968a0b472637c6130434e37f482b877
Mark Kwon <redhero8830@gmail.com>
2024-02-21 02:26:20 +0900

    modified api

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const API_KEY = "a5eea815621a4e13b2b7c866d42d0d8e";
 let news = [];
 const getLatestNews = async () => {
   const url = new URL(
-    `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?country=kr&apiKey=${API_KEY}`
+    // `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?country=kr&apiKey=${API_KEY}`
     // `https://markwon-jsstudy-news.netlify.app//top-headlines`
-    // `https://markwon-jsstudy-news.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`
+    `https://markwon-jsstudy-news.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`
   );
   const response = await fetch(url);
   const data = await response.json();
