08a67c51a6eb272c75fa842594cc678d424f44de
Mark Kwon <redhero8830@gmail.com>
2024-02-22 00:40:40 +0900

    render api

--- app.js
+++ app.js
@@ -1,46 +1,73 @@
-const author = document.querySelector(".author");
-const title = document.querySelector(".author");
+const API_KEY = `a5eea815621a4e13b2b7c866d42d0d8e`;
 
-const API_KEY = "a5eea815621a4e13b2b7c866d42d0d8e";
-let news = [];
+const BBC_url = new URL(
+  `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=a5eea815621a4e13b2b7c866d42d0d8e`
+);
+const noona_url = new URL(
+  `https://markwon-jsstudy-news.netlify.app/top-headlines`
+);
+
+let newList = [];
+
 const getLatestNews = async () => {
-  const url = new URL(
-    // `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?country=kr&apiKey=${API_KEY}`
-    // `https://markwon-jsstudy-news.netlify.app//top-headlines`
-    `https://markwon-jsstudy-news.netlify.app/top-headlines`
-  );
-  const response = await fetch(url);
-  const data = await response.json();
-  return data.articles;
+  const resource = await fetch(BBC_url);
+  const data = await resource.json();
+  newList = data.articles;
+
+  render();
+
+  console.log(newList);
 };
 
-getLatestNews().then((articles) => {
-  //   authors.innerText = news.author;
-  for (let article of articles) {
-    const liAuthor = document.createElement("li");
-    liAuthor.innerText = article.author;
-    author.append(liAuthor);
+const render = () => {
+  const showNews = newList
+    .map((news) => {
+      return `<div class="row news">
+    <div class="col-lg-4">
+      <img class="news-img" src="${news.urlToImage}" alt="">
+    </div>
+    <div class="col-lg-8 context">
+      <h2>
+        ${news.title}
+      </h2>
+      <p>
+      ${news.description}
+      </p>
+      <div>
+      ${news.publishedAt}
+      </div>
+    </div>
+  </div>`;
+    })
+    .join("");
 
-    const liTitle = document.createElement("li");
-    liTitle.innerText = article.title;
-    title.append(liTitle);
-  }
-});
-// // const url = new URL(
-// //   `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
-// // );
-// // const getLatestNews = () => {
-// //   const response = fetch(url);
-// //   response
-// //     .then((res) => {
-// //       return res.json();
-// //     })
-// //     .then((data) => {
-// //       console.log(data.articles[0].author);
-// //     })
-// //     .catch(() => {
-// //       console.log("ERROR");
-// //     });
-// // };
+  document.querySelector(".news-board").innerHTML = showNews;
+};
 
-// // getLatestNews();
+getLatestNews();
+
+const render2 = () => {
+  const showNews = newList
+    .map(
+      (news) => `<div class="row news">
+        <div class="col-lg-4">
+            <img class="news-img-size"
+                src=${news.urlToImage} alt="">
+        </div>
+        <div class="col-lg-8 context">
+            <h2>
+              ${news.title}
+            </h2>
+            <p>
+              ${news.description}
+            </p>
+            <div>
+            ${news.source.name} * ${news.publishedAt}
+            </div>
+        </div>
+      </div>`
+    )
+    .join("");
+
+  document.querySelector(".news-board").innerHTML = showNews;
+};
