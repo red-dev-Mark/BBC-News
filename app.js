7a72d7d2aeb7ba56758a1cf58864b180687eb17c
Mark Kwon <redhero8830@gmail.com>
2024-02-22 01:39:58 +0900

    filter

--- app.js
+++ app.js
@@ -1,30 +1,56 @@
 const API_KEY = `a5eea815621a4e13b2b7c866d42d0d8e`;
 
 const BBC_url = new URL(
-  `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=a5eea815621a4e13b2b7c866d42d0d8e`
+  `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${API_KEY}`
+);
+const Times_url = new URL(
+  `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
 );
 const noona_url = new URL(
   `https://markwon-jsstudy-news.netlify.app/top-headlines`
 );
 
 let newList = [];
+const keyword = document.querySelector("#search-input");
 const menus = document.querySelectorAll(".buttons button");
-menus.forEach((menu) => {
-  menu.addEventListener("click", (e) => getNewsByCategoty(e));
-});
+menus.forEach((menu) =>
+  menu.addEventListener("click", (e) => getNewsByCategoty(e))
+);
 
 const getLatestNews = async () => {
-  const resource = await fetch(noona_url);
-  const data = await resource.json();
-  newList = data.articles;
+  const response = await fetch(noona_url);
+  const data = await response.json();
+  newsList = data.articles;
 
   render();
+};
 
-  console.log(newList);
+const getNewsByCategoty = async (e) => {
+  const category = e.target.textContent.toLowerCase();
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  newsList = data.articles;
+
+  render();
+};
+
+const getNewsByKeyword = async () => {
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=us&q=${keyword.value}&apiKey=${API_KEY}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  newsList = data.articles;
+
+  render();
+  keyword.value = "";
 };
 
 const render = () => {
-  const showNews = newList
+  const showNews = newsList
     .map((news) => {
       return `<div class="row news">
     <div class="col-lg-4">
@@ -49,29 +75,3 @@ const render = () => {
 };
 
 getLatestNews();
-
-const render2 = () => {
-  const showNews = newList
-    .map(
-      (news) => `<div class="row news">
-        <div class="col-lg-4">
-            <img class="news-img-size"
-                src=${news.urlToImage} alt="">
-        </div>
-        <div class="col-lg-8 context">
-            <h2>
-              ${news.title}
-            </h2>
-            <p>
-              ${news.description}
-            </p>
-            <div>
-            ${news.source.name} * ${news.publishedAt}
-            </div>
-        </div>
-      </div>`
-    )
-    .join("");
-
-  document.querySelector(".news-board").innerHTML = showNews;
-};
